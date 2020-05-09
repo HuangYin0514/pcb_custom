0de48f6e85e959d5bec773ef3e05162bf7433a14
Huang Yin <1071324110@qq.com>
2020-05-09 14:08:38 +0800

    Resnet_self_attention

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from torchvision import models
+from model_utils import *
 
 
 class PCBModel(nn.Module):
@@ -144,9 +145,60 @@ class ResnetModel(nn.Module):
         return logits_list
 
 
+class Resnet_self_attention_Model(nn.Module):
+    def __init__(self,
+                 num_classes,
+                 loss='softmax',
+                 ** kwargs):
+
+        super(Resnet_self_attention_Model, self).__init__()
+        self.num_classes = num_classes
+        self.loss = loss
+
+        resnet = models.resnet50(pretrained=True)
+        # Modifiy the stride of last conv layer
+        resnet.layer4[0].conv2 = nn.Conv2d(
+            512, 512, kernel_size=3, bias=False, stride=1, padding=1)
+        resnet.layer4[0].downsample = nn.Sequential(
+            nn.Conv2d(1024, 2048, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(2048))
+
+        # Remove avgpool and fc layer of resnet
+        modules = list(resnet.children())[:-2]
+        self.backbone = nn.Sequential(*modules)
+
+        # Add new layers
+        self.sa = Self_Attn(2048)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.classfier = nn.Linear(2048, num_classes)
+
+    def forward(self, x):
+        resnet_features = self.backbone(x)
+        sa_features = self.sa(resnet_features)
+        features = self.avgpool(resnet_features)
+        features = features.view(x.size(0), -1)
+
+        if not self.training:
+            return features
+
+        logits = self.classfier(features)
+
+        if self.loss == 'softmax':
+            return logits
+        elif self.loss == 'triplet':
+            return logits, features.view(x.size(0), -1)
+        else:
+            raise KeyError("Unsupported loss: {}".format(self.loss))
+
+        return logits_list
+
 ##########
 # Instantiation
 ##########
+
+
 def PCB_p6(num_classes, share_conv, num_stripes=6, **kwargs):
     assert num_stripes == 6, "num_stripes not eq 6"
     return PCBModel(
@@ -163,10 +215,16 @@ def Res_net(num_classes,  **kwargs):
         **kwargs
     )
 
+def Resnet_self_attention(num_classes,  **kwargs):
+    return Resnet_self_attention_Model(
+        num_classes=num_classes,
+        **kwargs
+    )
 
 __Models_custom = {
     'PCB_p6': PCB_p6,
-    'Res_net': Res_net
+    'Res_net': Res_net,
+    'Resnet_self_attention':Resnet_self_attention
 }
 
 
@@ -180,7 +238,7 @@ def build_model(name, num_classes, **kwargs):
 
 
 if __name__ == "__main__":
-    model = build_model('Res_net', num_classes=6)
+    model = build_model('Resnet_self_attention', num_classes=6)
     print(model)
     # test input and ouput
     input = torch.randn(4, 3, 384, 128)
